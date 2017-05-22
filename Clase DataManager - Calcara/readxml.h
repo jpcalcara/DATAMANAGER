68963fbdad85767c3a362f3314da302752f30899
Julian Macagno <julian.macagno55@gmail.com>
2017-05-22 19:17:34 -0300

    Add files via upload

---
+++ Clase DataManager - Calcara/readxml.h
@@ -0,0 +1,44 @@
+#ifndef READXML_H
+#define READXML_H
+
+#include <QWidget>
+#include <QXmlStreamReader>
+#include <QTextStream>
+#include <QtXml/QDomDocument>
+#include <QtXml/QDomElement>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+namespace Ui {
+class ReadXml;
+}
+
+class ReadXml : public QWidget // tiene que heredad de QObject porque no usa interfaz esta clase
+{
+    Q_OBJECT
+
+public:
+
+    explicit ReadXml(QWidget *parent = 0);
+    void pedirTiempo();
+    void pedirDistancia();
+    void pedirPartida();
+    void pedirLlegada();
+
+    ~ReadXml();
+
+private:
+    Ui::ReadXml *ui;
+    QDomDocument *xmltemporal;
+    QNetworkAccessManager *manager;
+signals:
+    void signaTiempo(QString);
+    void signalDistancia(QString);
+    void signalPartida(QString);
+    void signalLlegada(QString);
+
+private slots:
+    void slot_abrirXML();
+    void slot_xmlDownload(QNetworkReply *reply);
+};
+
+#endif // READXML_H
