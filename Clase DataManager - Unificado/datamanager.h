1619b153a1edbc2d7a94fceec3b908f0933d404a
JotaPeCode <jp.calcara@gmail.com>
2017-06-05 15:27:50 -0300

    Update datamanager.h

--- Clase DataManager - Unificado/datamanager.h
+++ Clase DataManager - Unificado/datamanager.h
@@ -26,8 +26,8 @@ public:
 
     //void getTiempo();
     //void getDistancia();
-    void getOrigen(QString start);
-    void getDestino(QString end);
+    void getOrigen(QString start);// permite obtener la latitud y longitud del origen
+    void getDestino(QString end); //permite obtener la latitud y longitud del destino
     void getCoordinates(QString street);
     void getSites(QString search, QList<QString> coord);
 
@@ -37,8 +37,8 @@ private:
 private slots:
     void slot_getCoordinates(QNetworkReply *reply);
     void slot_getSites(QNetworkReply *reply);
-    void slot_getTiempo(QNetworkReply *reply);
-    void slot_getDistancia(QNetworkReply *reply);
+    void slot_getTiempo(QNetworkReply *reply);//falta asignar lectura
+    void slot_getDistancia(QNetworkReply *reply);//falta asignar lectura
     void slot_getOrigen(QNetworkReply *reply);
     void slot_getDestino(QNetworkReply *reply);
 
@@ -46,8 +46,8 @@ private slots:
 signals:
     void receivedCoordinates(QList<QString>);
     void receivedSites(QList<QString>);
-    //void signalTiempo(QString, QString);
-   // void signalDistancia(QString, QString);
+    //void signalTiempo(QString, QString); //falta asignar lectura
+   // void signalDistancia(QString, QString); //falta asignar lectura
     void signalOrigen(QString, QString);
     void signalDestino(QString, QString);
 };
